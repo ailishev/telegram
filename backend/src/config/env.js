4dbe675ac00e41cd511318e4905e20bd1e045515
ailishev <114398805+ailishev@users.noreply.github.com>
2026-05-10 21:36:08 +0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({path: path.resolve(__dirname, '../../.env')});
 
 export const env = {
-  port: Number(process.env.PORT || 8000),
+  port: Number(8000),
   sessionTtlMs: 1000 * 60 * 60 * 24 * 30,
   otpTtlMs: 1000 * 60 * 5,
   cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
